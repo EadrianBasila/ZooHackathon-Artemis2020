010cf753be95f0cd47c44943339422154acada39
Jurie Mae Castronuevo <juriemaeac@users.noreply.github.com>
2020-11-06 19:48:53 +0800

    login account

--- FB_webScraper.py
+++ FB_webScraper.py
@@ -1,2 +1,17 @@
 #hi
 #testing connection
+
+from selenium.webdriver.common.keys import Keys
+from msedge.selenium_tools import Edge, EdgeOptions
+
+options = EdgeOptions()
+options.use_chromium = True
+driver = Edge(options=options)
+
+# navigate to login screen
+driver.get('https://www.facebook.com/login')
+driver.maximize_window()
+
+#login account
+email = driver.find_element_by_name('email').send_keys('artemis11620@gmail.com')
+password = driver.find_element_by_name('pass').send_keys('@rtemiS1120', Keys.RETURN)
